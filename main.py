adc14c6bfbdaebe2b3312c5a6d42eee136ba47eb
Anton Voronin <lovemoto24@mail.ru>
2022-08-13 22:45:09 +0300

    add readme and comments

--- main.py
+++ main.py
@@ -18,11 +18,13 @@ count1 = Counter()
 count2 = Counter()
 count3 = Counter()
 
+
 text_list = {
     1: text1,
     2: text2,
     3: text3
 }
+# Считаем кол-во строк в йале по кол-ву спец. символов
 for i in file1:
     count1['\n'] += 1
 for i in file2:
@@ -30,15 +32,17 @@ for i in file2:
 for i in file3:
     count3['\n'] += 1
 
+# Создаем словарь с именем файлов и кол-вом строк в нем
 count_string = {}
 count_string['1.txt'] = count1['\n']
 count_string['2.txt'] = count2['\n']
 count_string['3.txt'] = count3['\n']
 
-
+# Сортируем словарь по значениям и перезаписываем словарь
 sorted_tuple = sorted(count_string.items(), key=lambda x: x[1])
 count_string = dict(sorted_tuple)
 
+# Запускать только 1 раз, т.к. по заданию необходимо лишь записать файл с определенной структурой
 with open('new file.txt.', mode='a+', encoding='utf-8') as f:
 
     for key, value in count_string.items():
